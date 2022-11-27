f0589cfdfa7eac8c6fb322007cc65d4ce594c782
yakura12kk <yakura12kk@gmail.com>
2022-11-27 19:42:39 +0700

    Crash fix

--- app/src/main/java/com/white/meters/MainVM.kt
+++ app/src/main/java/com/white/meters/MainVM.kt
@@ -3,16 +3,22 @@ package com.white.meters
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.white.meters.data.base.InitRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
+@HiltViewModel
 class MainVM @Inject constructor(
     private val initRepository: InitRepository
 ) : ViewModel() {
 
     fun init() {
         viewModelScope.launch {
-            initRepository.init()
+            try {
+                initRepository.init()
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
         }
     }
 }
--- app/src/main/java/com/white/meters/data/repository/InitRepositoryImpl.kt
+++ app/src/main/java/com/white/meters/data/repository/InitRepositoryImpl.kt
@@ -2,20 +2,25 @@ package com.white.meters.data.repository
 
 import com.white.meters.data.base.Consts
 import com.white.meters.data.base.InitRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import okhttp3.*
 import okhttp3.MediaType.Companion.toMediaType
+import javax.inject.Inject
 
-class InitRepositoryImpl() : InitRepository {
+class InitRepositoryImpl @Inject constructor() : InitRepository {
 
     override suspend fun init() {
-        val client = OkHttpClient().newBuilder()
-            .build()
-        val mediaType = "text/plain".toMediaType()
-        val body: RequestBody = RequestBody.create(mediaType, "")
-        val request: Request = Request.Builder()
-            .url("${Consts.BASE_URL}${Consts.INIT_ENDPOINT}")
-            .method("POST", body)
-            .build()
-        val response: Response = client.newCall(request).execute()
+        withContext(Dispatchers.IO) {
+            val client = OkHttpClient().newBuilder()
+                .build()
+            val mediaType = "text/plain".toMediaType()
+            val body: RequestBody = RequestBody.create(mediaType, "")
+            val request: Request = Request.Builder()
+                .url("${Consts.BASE_URL}${Consts.INIT_ENDPOINT}")
+                .method("POST", body)
+                .build()
+            val response: Response = client.newCall(request).execute()
+        }
     }
 }
